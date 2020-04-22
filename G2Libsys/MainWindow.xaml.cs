7a4cb5f3a9de51962732bf61f13ce7a089d7d826
CM <christofferjcm@gmail.com>
2020-04-22 10:14:13 +0200

    created mainwindow view presenter

--- G2Libsys/MainWindow.xaml.cs
+++ G2Libsys/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using G2Libsys.ViewModels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -23,6 +24,8 @@ namespace G2Libsys
         public MainWindow()
         {
             InitializeComponent();
+
+            DataContext = new MainWindowViewModel();
         }
     }
 }
