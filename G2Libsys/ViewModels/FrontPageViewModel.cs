47a02608b9e308ab6c2f4075533a67c832556752
CM <christofferjcm@gmail.com>
2020-04-22 10:12:45 +0200

    created basic viewmodels

---
+++ G2Libsys/ViewModels/FrontPageViewModel.cs
@@ -0,0 +1,22 @@
+﻿using G2Libsys.Commands;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Input;
+
+namespace G2Libsys.ViewModels
+{
+    public class FrontPageViewModel : BaseViewModel
+    {
+        public virtual ICommand NavigateToVM { get; protected set; }
+
+        public FrontPageViewModel()
+        {
+            NavigateToVM = new RelayCommand<Type>(vm =>
+            {
+                // Create new ViewModel
+                MainWindowViewModel.HostScreen.CurrentViewModel = Activator.CreateInstance(vm);
+            }/*, a => { return CurrentViewModel == null; }*/);
+        }
+    }
+}
---
+++ G2Libsys/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,39 @@
+﻿using G2Libsys.Commands;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace G2Libsys.ViewModels
+{
+    public class MainWindowViewModel : BaseViewModel
+    {
+        public static MainWindowViewModel HostScreen { get; set; }
+
+        private object currentViewModel;
+
+        public object CurrentViewModel
+        {
+            get => currentViewModel;
+            set
+            {
+                currentViewModel = value;
+                OnPropertyChanged(nameof(CurrentViewModel));
+            }
+        }
+
+        public MainWindowViewModel()
+        {
+
+            //NavigateToVM = new RelayCommand<FrontPageViewModel>(vm =>
+            //{
+            //    // Create new ViewModel
+            //    CurrentViewModel = Activator.CreateInstance(typeof(FrontPageViewModel), new object[] { vm });
+            //});
+
+            //CurrentViewModel = Activator.CreateInstance(typeof(FrontPageViewModel));            
+            CurrentViewModel = new FrontPageViewModel();
+
+            HostScreen = this;
+        }
+    }
+}
