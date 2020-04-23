22327e82fe23bbcc020a8c9a4bcefe769449ccb5
CM <christofferjcm@gmail.com>
2020-04-23 11:34:28 +0200

    generic repository

---
+++ G2Libsys.Data/Repository/Base/GenericRepository.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Dapper;
+using DapperExtensions;
+
+namespace G2Libsys.Data.Repository
+{
+    public abstract class GenericRepository<T> : IRepository<T> where T : class
+    {
+        private readonly string _tableName;
+        private readonly string _connectionString;
+
+        /// <summary>
+        /// tableName = target table in database
+        /// </summary>
+        public GenericRepository(string tableName)
+        {
+            _tableName = tableName;
+            _connectionString = ConfigurationManager.ConnectionStrings["sqldefault"].ConnectionString;
+        }
+
+        protected IDbConnection Connection => new SqlConnection(_connectionString);
+
+        // Create
+        public async Task<int> AddAsync(T item)
+        {
+            using var _db = Connection;
+            // Example: _tableName = "users";
+            // usp_insert_users = procedure name
+            return await _db.ExecuteScalarAsync<int>("usp_insert_" + _tableName, item, commandType: CommandType.StoredProcedure);
+        }
+
+        public async Task AddRange(IEnumerable<T> item)
+        {
+            using var _db = Connection;
+            using var transaction = _db.BeginTransaction();
+            try
+            {
+                await _db.ExecuteAsync("usp_insertrange_" + _tableName, item, commandType: CommandType.StoredProcedure, transaction: transaction);
+                transaction.Commit();
+            }
+            catch
+            {
+                transaction.Rollback();
+                throw new Exception("Insert Failed");
+            }
+        }
+
+        // Read
+        public async Task<T> GetByIdAsync(int id)
+        {
+            using var _db = Connection;
+            return await _db.QueryFirstAsync<T>("usp_getbyid_" + _tableName, new { id }, commandType: CommandType.StoredProcedure);
+        }
+
+        public async Task<IEnumerable<T>> GetAllAsync()
+        {
+            using var _db = Connection;
+            return await _db.QueryAsync<T>("usp_getall_" + _tableName, new { }, commandType: CommandType.StoredProcedure);
+        }
+
+        // Update
+        public async Task UpdateAsync(T item)
+        {
+            using var _db = Connection;
+            await _db.ExecuteAsync("usp_update_" + _tableName, item, commandType: CommandType.StoredProcedure);
+        }
+
+        // Delete
+        public async Task RemoveAsync(T item)
+        {
+            using var _db = Connection;
+            await _db.ExecuteAsync("usp_remove_" + _tableName, item, commandType: CommandType.StoredProcedure);
+        }
+    }
+}
---
+++ G2Libsys.Data/Repository/Base/IRepository.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace G2Libsys.Data.Repository
+{
+    public interface IRepository<T>
+    {
+        /// <summary>
+        /// Insert new item
+        /// </summary>
+        /// <param name="item"></param>
+        /// <returns></returns>
+        public Task<int> AddAsync(T item);
+
+        /// <summary>
+        /// Insert multiple items
+        /// </summary>
+        /// <param name="item"></param>
+        /// <returns></returns>
+        public Task AddRange(IEnumerable<T> item);
+
+        /// <summary>
+        /// Get item by id
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        public Task<T> GetByIdAsync(int id);
+
+        /// <summary>
+        /// Get all items
+        /// </summary>
+        /// <returns></returns>
+        public Task<IEnumerable<T>> GetAllAsync();
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="item"></param>
+        /// <returns></returns>
+        public Task UpdateAsync(T item);
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="item"></param>
+        /// <returns></returns>
+        public Task RemoveAsync(T item);
+    }
+}
