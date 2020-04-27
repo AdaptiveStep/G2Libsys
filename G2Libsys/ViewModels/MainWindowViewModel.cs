3d24e4d9d5633c7909d50f15fac8bbf50671eb64
CM <christofferjcm@gmail.com>
2020-04-28 01:42:01 +0200

    base crud repo done

--- G2Libsys/ViewModels/MainWindowViewModel.cs
+++ G2Libsys/ViewModels/MainWindowViewModel.cs
@@ -12,8 +12,6 @@ namespace G2Libsys.ViewModels
         public static MainWindowViewModel HostScreen { get; set; }
 
         private object currentViewModel;
-        private readonly IRepository _repository;
-        private readonly IUserRepository _userRepo;
 
         public object CurrentViewModel
         {
@@ -27,32 +25,7 @@ namespace G2Libsys.ViewModels
 
         public MainWindowViewModel()
         {
-            // Exempelkod använder temporär databas
-            _repository = new GeneralRepository();
-            _userRepo = new UserRepository();
 
-            GetUsers();
-            InsertUser();
-
-            // Initial viewmodel 
-            //CurrentViewModel = new FrontPageViewModel();
-
-            //HostScreen = this;
-        }
-
-        // Exempelkod använder temporär databas
-        private async void GetUsers()
-        {
-            List<User> userlist = new List<User>(await _userRepo.GetAllAsync());
-            List<User> list = new List<User>(await _repository.GetAllAsync<User>());
-        }
-
-        private async void InsertUser()
-        {
-            var user = new User { Email = "bob5@g2systems.com", Firstname = "Bob", Lastname = "Ross", Password = "123" };
-            var user2 = new User { Email = "bert3@g2systems.com", Firstname = "Bert", Lastname = "Karlsson", Password = "123" };
-            user.ID = await _userRepo.AddAsync(user);
-            user2.ID = await _repository.AddAsync(user2);
         }
     }
 }
