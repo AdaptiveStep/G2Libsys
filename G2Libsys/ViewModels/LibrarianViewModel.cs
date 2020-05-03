c6f7c8dad64f778f45b3b45a90a2f468b69f2ac6
sebastian melin <melin46@gmail.com>
2020-05-03 19:05:52 +0200

    knappar tillagda

--- G2Libsys/ViewModels/LibrarianViewModel.cs
+++ G2Libsys/ViewModels/LibrarianViewModel.cs
@@ -1,15 +1,15 @@
 ﻿using G2Libsys.Data.Repository;
 using G2Libsys.Library;
+using G2Libsys.Commands;
 using System;
-using System.Collections.Generic;
 using System.Collections.ObjectModel;
-using System.Text;
-
 
 namespace G2Libsys.ViewModels
 {
     public class LibrarianViewModel: BaseViewModel
     {
+        public RelayCommand addbutton { get; private set; }
+        public RelayCommand deletebutton { get; private set; }
         private readonly GeneralRepository<User> _repo;
 
         //binda knapparna från viewen
@@ -23,19 +23,37 @@ namespace G2Libsys.ViewModels
         }
         public LibrarianViewModel()
         {
+            addbutton = new RelayCommand(x => AddUser());
+            deletebutton = new RelayCommand(DeleteUser);
             _repo = new GeneralRepository<User>();
             GetUsers();
         }
         
         public async void GetUsers()
         {
-            await _repo.AddRange<User>(Users);
-            
+            Users = new ObservableCollection<User>(await _repo.GetAllAsync());
+
+        }
+
+        
+        public async void DeleteUser(object parameter)
+        {
+            await _repo.RemoveAsync(parameter);
+        }
+        public async void AddUser()
+        {
+            //await _repo.AddAsync();
         }
         //lista av besökare
         //ta bort besökare funktion
         //lägga till besökare funktion
         //söka efter besökare. antigen en funtion eller i datagrid
         //registrera lånekort
+
+        public string name;
+        public string lastname;
+        public string email;
+        public int ssnumber;
+        public string remail;
     }
 }
--- G2Libsys/Views/LibrarianView.xaml.cs
+++ G2Libsys/Views/LibrarianView.xaml.cs
@@ -1,5 +1,4 @@
 ﻿using G2Libsys.ViewModels;
-using MaterialDesignThemes.Wpf;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -10,7 +9,6 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace G2Libsys.Views
