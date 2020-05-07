28edcc5c5b6719922525a6868c5cf49921480a62
CM <christofferjcm@gmail.com>
2020-05-07 03:35:54 +0200

    created navservice

---
+++ G2Libsys/Services/NavService.cs
@@ -0,0 +1,61 @@
+﻿using G2Libsys.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace G2Libsys.Services
+{
+    public static class NavService
+    {
+        public static IHostScreen HostScreen { get; private set; }
+
+        public static void Setup(IHostScreen vm)
+        {
+            HostScreen = vm;
+        }
+
+        private static List<BaseViewModel> ViewModels { get; set; }
+
+        public static BaseViewModel Locate(this Type vm)
+        {
+            if (ViewModels is null) ViewModels = new List<BaseViewModel>();
+
+            return ViewModels.Where(x => x.GetType().Name == vm.Name).FirstOrDefault();
+        }
+
+        public static void GoToAndReset(BaseViewModel vm)
+        {
+            HostScreen.CurrentViewModel = vm;
+            ViewModels.Clear();
+        }
+
+        public static BaseViewModel GoToNewInstance(BaseViewModel vm)
+        {
+            var viewModel = vm.GetType().Locate();
+
+            if (viewModel != null)
+            {
+                ViewModels.Remove(viewModel);
+            }
+
+            ViewModels.Add(vm);
+
+            return vm;
+        }
+
+        public static BaseViewModel GoTo(BaseViewModel vm)
+        {
+            var viewModel = vm.GetType().Locate();
+
+            if (viewModel != null)
+            {
+                return viewModel;
+            }
+
+            ViewModels.Add(vm);
+
+            return vm;
+        }
+    }
+}
--- G2Libsys/ViewModels/MainWindowViewModel.cs
+++ G2Libsys/ViewModels/MainWindowViewModel.cs
@@ -2,6 +2,7 @@
 using G2Libsys.Data.Repository;
 using G2Libsys.Library;
 using G2Libsys.Models;
+using G2Libsys.Services;
 using G2Libsys.Views;
 using System;
 using System.Collections.Generic;
@@ -14,24 +15,20 @@ using System.Windows.Input;
 
 namespace G2Libsys.ViewModels
 {
-    public class MainWindowViewModel : BaseViewModel
+    public class MainWindowViewModel : BaseViewModel, IHostScreen
     {
         #region Private Fields
 
         private readonly IRepository _repo;
         private User currentUser;
         private UserMenuItem menuItem;
-        private object currentViewModel;
+        private ObservableCollection<UserMenuItem> menuItems;
+        private BaseViewModel currentViewModel;
         private bool developerMode = false;
 
         #endregion
 
         #region Public Properties
-
-        /// <summary>
-        /// Property for calling the mainviewmodel for navigation purposes
-        /// </summary>
-        public static MainWindowViewModel HostScreen { get; set; }
 
         /// <summary>
         /// Viewmodels for developer menu
@@ -68,10 +65,20 @@ namespace G2Libsys.ViewModels
             }
         }
 
+        public ObservableCollection<UserMenuItem> MenuItems
+        {
+            get => menuItems;
+            set
+            {
+                menuItems = value;
+                OnPropertyChanged(nameof(MenuItems));
+            }
+        }
+
         /// <summary>
         /// Sets the active viewmodel
         /// </summary>
-        public object CurrentViewModel
+        public BaseViewModel CurrentViewModel
         {
             get => currentViewModel;
             set
@@ -112,7 +119,7 @@ namespace G2Libsys.ViewModels
         /// <summary>
         /// Call logout method
         /// </summary>
-        public ICommand LogOutCommand { get; set; }
+        public ICommand LogOutCommand { get; private set; }
 
         #endregion
 
@@ -137,7 +144,7 @@ namespace G2Libsys.ViewModels
             SetDevViewModels();
 
             // Set MainWindowViewModel to hostscreen
-            HostScreen = this;
+            //HostScreen = this;
 
             // Initial viewmodel 
             CurrentViewModel = new LibraryMainViewModel();
