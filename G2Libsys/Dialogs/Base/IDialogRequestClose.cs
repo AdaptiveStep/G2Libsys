8d3572784e2e0cdd340defbdd0199a8487d9dadc
CM <christofferjcm@gmail.com>
2020-05-11 03:41:33 +0200

    created dialogclose event

---
+++ G2Libsys/Dialogs/Base/IDialogRequestClose.cs
@@ -0,0 +1,10 @@
+﻿using G2Libsys.Events;
+using System;
+
+namespace G2Libsys.Dialogs
+{
+    public interface IDialogRequestClose
+    {
+        event EventHandler<DialogCloseRequestedEventArgs> CloseRequested;
+    }
+}
