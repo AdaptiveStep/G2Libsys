9f9e959d8c2ee9584711e9586cccd60bab29b228
sebastian melin <melin46@gmail.com>
2020-05-28 13:08:02 +0200

    fungerande

--- G2Libsys/Dialogs/ReasonDialogViewModel.cs
+++ G2Libsys/Dialogs/ReasonDialogViewModel.cs
@@ -1,10 +1,50 @@
-﻿using System;
+﻿using G2Libsys.Commands;
+using G2Libsys.Library;
+using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Windows.Input;
 
 namespace G2Libsys.Dialogs
 {
-    class ReasonDialogViewModel
+    public class ReasonDialogViewModel : BaseDialogViewModel<string>
     {
+
+        private string reason;
+        public string Reason
+        {
+            get => reason;
+            set
+            {
+                reason = value;
+                OnPropertyChanged(nameof(Reason));
+            }
+        }
+        //private Card card;
+        //public Card Card
+        //{
+        //    get => card;
+        //    set
+        //    {
+        //        card = value;
+        //        OnPropertyChanged(nameof(Card));
+        //    }
+        //}
+
+        public ICommand SaveCommand => new RelayCommand(Result);
+        private void Result(object param = null)
+        {
+
+
+            base.DialogResult = Reason;
+            base.OKCommand.Execute(param);
+        }
+        public ReasonDialogViewModel(string Reason,  string title = null)
+            : base(title)
+        {
+            this.Reason = Reason;
+
+        }
     }
 }
+
